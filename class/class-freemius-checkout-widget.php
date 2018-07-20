52aad3bf8a76a6ae9cfe19ff27b285a444ad06db
sebastienserre <sebastien@thivinfo.com>
2018-07-20 19:27:58 +0200

    initial commit

---
+++ class/class-freemius-checkout-widget.php
@@ -0,0 +1,128 @@
+<?php
+
+class Freemius_Checkout_Widget extends WP_Widget {
+	public function __construct() {
+		$widget_args = array(
+			'classname'   => 'Freemius Checkout Widget',
+			'description' => __( 'Add a Button Freemius Checkout every where in WordPress', 'checkout-freemius-rewamped' ),
+		);
+		parent::__construct(
+			'freemius-checkout-widget',
+			'Freemius Checkout Widget',
+			$widget_args
+		);
+		add_action( 'widgets_init', array( $this, 'freemius_checkout_init_widget' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'freemius_load_script' ) );
+	}
+
+	public function freemius_checkout_init_widget() {
+		register_widget( 'Freemius_Checkout_Widget' );
+	}
+
+	public function form( $instance ) {
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_name( 'title' ); ?>"> <?php esc_attr_e( 'Title:', 'checkout-freemius-rewamped' ); ?>
+			</label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
+			       name="<?php echo $this->get_field_name( 'title' ); ?>" type="text"
+			       value="<?php echo $instance['title']; ?>"/>
+		</p>
+			<label for="<?php echo $this->get_field_name( 'description' ); ?>"> <?php esc_attr_e( 'Description:', 'checkout-freemius-rewamped' ); ?>
+			</label>
+			<textarea class="widefat" id="<?php echo $this->get_field_id( 'description' ); ?>"
+			       name="<?php echo $this->get_field_name( 'description' ); ?>"
+			          ><?php echo $instance['description']; ?></textarea>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_name( 'plugin_id' ); ?>"> <?php esc_attr_e( 'plugin_id:', 'checkout-freemius-rewamped' ); ?>
+			</label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'plugin_id' ); ?>"
+			       name="<?php echo $this->get_field_name( 'plugin_id' ); ?>" type="text"
+			       value="<?php echo $instance['plugin_id']; ?>"/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_name( 'plan_id' ); ?>"> <?php esc_attr_e( 'plan_id:', 'checkout-freemius-rewamped' ); ?>
+			</label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'plan_id' ); ?>"
+			       name="<?php echo $this->get_field_name( 'plan_id' ); ?>" type="text"
+			       value="<?php echo $instance['plan_id']; ?>"/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_name( 'public_key' ); ?>"> <?php esc_attr_e( 'public_key:', 'checkout-freemius-rewamped' ); ?>
+			</label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'public_key' ); ?>"
+			       name="<?php echo $this->get_field_name( 'public_key' ); ?>" type="text"
+			       value="<?php echo $instance['public_key']; ?>"/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_name( 'button_text' ); ?>"> <?php esc_attr_e( 'button text:', 'checkout-freemius-rewamped' ); ?>
+			</label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'button_text' ); ?>"
+			       name="<?php echo $this->get_field_name( 'button_text' ); ?>" type="text"
+			       value="<?php echo $instance['button_text']; ?>"/>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_name( 'image' ); ?>"> <?php esc_attr_e( 'image:', 'checkout-freemius-rewamped' ); ?>
+			</label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'image' ); ?>"
+			       name="<?php echo $this->get_field_name( 'image' ); ?>" type="text"
+			       value="<?php echo $instance['image']; ?>"/>
+		</p>
+
+		<?php
+	}
+	
+	public function widget( $args, $instance) {
+		echo $args['before_widget'];
+
+		echo $args['before_title'];
+
+		echo apply_filters( 'widget_title', $instance['title'] );
+
+		echo $args['after_title'];
+		if (! empty( $instance['description'] ) ){
+			echo '<p>' . $instance['description'] . '</p>';
+		}
+		?>
+<button id="purchase" class="purchase"><?php echo $instance['button_text']; ?></button>
+<?php
+		wp_enqueue_script('FreemiusJS');
+		?>
+
+<script>
+            var handler = FS.Checkout.configure({
+                plugin_id:  '<?php echo $instance['plugin_id']; ?>',
+                plan_id:    '<?php echo $instance['plan_id']; ?>',
+                public_key: '<?php echo $instance['public_key']; ?>',
+                image:      '<?php echo $instance['image']; ?>'
+            });
+
+            jQuery(document).ready(function($) {
+                $('.purchase').on('click', function (e) {
+                    handler.open({
+                        name: 'Test Product',
+                        licenses: $('#licenses').val(),
+                        // You can consume the response for after purchase logic.
+                        success: function (response) {
+                            // alert(response.user.email);
+                        }
+                    });
+                    e.preventDefault();
+                })
+            });
+		</script>
+<?php
+
+		//echo do_shortcode('[freemius_checkout name="OpenAgenda pour WordPress Pro" plugin_id="2279" plan_id="3475" pricing_id="2592" public_key="pk_ab0021b682585d81e582568095957" button_id="purchase" button="Essayer gratuitement pendant 14 jours" button_class="edd-add-to-cart button blue edd-submit edd-has-js"]');
+	//return ob_get_clean();
+	}
+
+	public function freemius_load_script() {
+		wp_register_script( 'FreemiusJS', 'https://checkout.freemius.com/checkout.min.js', array( 'jquery' ) );
+	}
+}
+
+new Freemius_Checkout_Widget();
+
+
---
+++ readme.txt
@@ -0,0 +1,74 @@
+=== Freemius Checkout Rewamped ===
+Contributors: sebastienserre
+Tags: freemius, checkout, buy button
+Requires at least: 4.0
+Tested up to: 4.9
+Stable tag: 1.0
+Requires PHP: 5.6
+License: GPLv3
+License URI: https://opensource.org/licenses/GPL-3.0
+
+Sell WordPress Plugins & Themes. Anywhere. Using Freemius Checkout "Buy Now" button.
+
+== Description ==
+
+[Freemius](https://freemius.com/) empowers WordPress plugin/theme developers to sell in minutes, helping them create prosperous subscription based businesses.
+
+With [Freemius Checkout](https://freemius.com/wordpress/checkout/) you can sell your digital WordPress products from anywhere.
+
+This plugin allows anyone selling WordPress plugins or themes to embed a "Buy Now" button anywhere in your site using a simple WordPress shortcode.
+
+When embedded, the user will see a simple "Buy Now" button on the front-end. Clicking the button will open the checkout popup.
+
+This Plugin is a fork of the original one [Checkout-Freemius](https://wordpress.org/plugins/checkout-freemius/) which add the possibility to add a button with WordPress Widget.
+
+
+= Usage =
+
+The plugin uses **[freemius_checkout]** shortcode to embed the "Buy Now" button.
+
+The shortcode is very customizable, to setup a custom product you can use the following attributes:
+
+* **name** - Your plugin/theme name.
+* **plugin_id** - Your plugin/theme ID on freemius.
+* **plan_id** - The plan you want to promote using the button.
+* **pricing_id** - The plan pricing level.
+* **public_key** - Plugin/theme public key.
+* **image** - The plugin/theme logo URL. Will be displayed in the popup.
+* **button** - The text to display on the button. Default is 'Buy Now'.
+* **button_id** - The button tag 'ID' attribute. Default is 'purchase'.
+* **button_class** - The button tag 'Class' attribute, to add your own styling.
+
+Example:
+
+`[freemius_checkout name="Press Elements" plugin_id="761" plan_id="1078" pricing_id="928" public_key="pk_fe2850d57f7d4f206aefaa106b91f" button_id="purchase" button="Buy Now"]`
+
+== Screenshots ==
+1. Freemius Checkout
+
+== Frequently Asked Questions ==
+
+= Why is the checkout button not working? =
+
+The theme needs to use jQuery to display the checkout popup. If it doesn't enqueue jQuery, the button won't open the checkout popup.
+
+= What are the plugin requirements? =
+
+**Minimum Requirements**
+
+* WordPress version 4.0 or greater.
+* PHP version 5.6 or greater.
+* MySQL version 5.0 or greater.
+
+**Recommended Requirements**
+
+* The latest WordPress version.
+* The latest PHP version.
+* MySQL version 5.7 or greater.
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial release.
+* Freemius checkout widget.
