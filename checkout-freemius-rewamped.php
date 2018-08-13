e2320f0c8f0ee7d28af234bfabb959201dec00bf
sebastienserre <sebastien@thivinfo.com>
2018-08-13 09:48:01 +0200

    add version number

--- checkout-freemius-rewamped.php
+++ checkout-freemius-rewamped.php
@@ -3,7 +3,7 @@
  * Plugin Name: Freemius Checkout Rewamped
  * Description: Sell WordPress Plugins & Themes. Anywhere. Using Freemius Checkout "Buy Now" button.
  * Plugin URI:  https://wordpress.org/plugins/checkout-freemius-rewamped/
- * Version:     1.1
+ * Version:     1.1.1
  * Author:      Sébastien Serre
  * Author URI:  https://thivinfo.com
  * Text Domain: checkout-freemius-rewamped
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Define Constant
  */
-define( 'PLUGIN_VERSION', '1.1.0' );
+define( 'PLUGIN_VERSION', '1.1.1' );
 define( 'FREEMIUS_CHECKOUT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'FREEMIUS_CHECKOUT_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'FREEMIUS_CHECKOUT_PLUGIN_DIR', untrailingslashit( FREEMIUS_CHECKOUT_PLUGIN_PATH ) );
